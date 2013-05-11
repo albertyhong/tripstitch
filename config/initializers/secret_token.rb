3503a05bd613bb106f40bac7f3414cb4efb11f76
Albert Hong <hong.albert@gmail.com>
2013-05-10 20:59:30 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tripstitch::Application.config.secret_token = '12ae30cf316e66bbffcac76fb983190cefd0be21cace6e41e3bacbe360c5bad1ef9bf4e7cb178f9b88de80b67a0c1da9e1158e510db67efbd1d86d359fbac63b'
---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Tripstitch::Application.routes.draw do
+  get "users/new"
+
+  get "users/create"
+
+  get "sessions/new"
+
+  get "sessions/create"
+
+  get "sessions/destroy"
+
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  get "log_out" => "sessions#destroy", :as => "log_out"
+
+  root :to => "users#new"
+
+  resources :users
+  resources :sessions
+end
